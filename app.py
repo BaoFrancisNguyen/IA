b88d6e119df7bc83d06c288d80cfb342f47aa318
BaoFrancisNguyen <https://github.com/BaoFrancisNguyen.git>
2022-08-12 19:42:30 +0200

    modif

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ class MyApp:
         self.window.title("My Application")
         self.window.geometry("720x480")
         self.window.minsize(480, 360)
-        self.window.iconbitmap("logo.ico")
+        #self.window.iconbitmap("/home/francis/Github/IA/logo.ico")
         self.window.config(background='#41B77F')
 
         # initialization des composants
@@ -24,24 +24,24 @@ class MyApp:
     def create_widgets(self):
         self.create_title()
         self.create_subtitle()
-        self.create_youtube_button()
+        self.create_generate_button()
 
     def create_title(self):
-        label_title = Label(self.frame, text="Bienvenue sur l'application", font=("Courrier", 40), bg='#41B77F',
+        label_title = Label(self.frame, text="PALANTIR", font=("Courrier", 40), bg='#41B77F',
                             fg='white')
         label_title.pack()
 
     def create_subtitle(self):
-        label_subtitle = Label(self.frame, text="Hey salut à tous c'est Graven", font=("Courrier", 25), bg='#41B77F',
+        label_subtitle = Label(self.frame, text="AI", font=("Courrier", 25), bg='#41B77F',
                                fg='white')
         label_subtitle.pack()
 
-    def create_youtube_button(self):
-        yt_button = Button(self.frame, text="Ouvrir Youtube", font=("Courrier", 25), bg='white', fg='#41B77F',
-                           command=self.open_graven_channel)
+    def create_generate_button(self):
+        yt_button = Button(self.frame, text="GENERATE", font=("Courrier", 25), bg='white', fg='#41B77F',
+                           command=self.open_channel)
         yt_button.pack(pady=25, fill=X)
 
-    def open_graven_channel(self):
+    def open_channel(self):
         webbrowser.open_new("http://youtube.com/gravenilvectuto")
 
 
